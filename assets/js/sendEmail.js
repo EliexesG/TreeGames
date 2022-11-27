f20b0b4338be4bc7209a56314c655ab3f45037e2
EliexesG <107147734+EliexesG@users.noreply.github.com>
2022-11-27 13:23:39 -0600

    Contacto terminado

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,10 +1,42 @@
 
-document.getElementById("contactar").addEventListener('click', () => {
-    sendEmail();
+$("#formulario").validate({
+    rules: {
+        email: {
+            required: true,
+            email: true
+        },
+        nombre: {
+            required: true,
+            minlength: 3,
+            maxlength: 30
+        },
+        fecha: {
+            required: true,
+            date: true
+        },
+        genero: {
+            required: true
+        },
+        mensaje: {
+            required: true,
+            minlength: 5,
+            maxlength: 200
+        }
+    }
 });
 
-function sendEmail() {
+document.getElementById("contactar").addEventListener('click', async () => {
+    
+    if($("#formulario").valid() == false){
+        return;
+    }
 
+    await sendEmail();
+});
+
+async function sendEmail() {
+
+    var ok = false;
 
     let email = $("#email").val();
     let nombre = $("#nombre").val();
@@ -38,45 +70,53 @@ function sendEmail() {
     })
 
 
-    // Email.send({
-    //     Host: "smtp.elasticemail.com",
-    //     Username: "eliasgabriel6100@gmail.com",
-    //     Password: "C94FBD417510DC4B19F0074F32A2E4DF6336",
-    //     To: 'eliasgabriel6100@gmail.com',
-    //     From: "nodemailerproyecto@gmail.com",
-    //     Subject: "This is the subject",
-    //     Body: `<table border="1">
-    //                     <thead>
-    //                         <th>Email</th>
-    //                         <th>Nombre</th>
-    //                         <th>Fecha</th>
-    //                         <th>Rango</th>
-    //                         <th>Genero</th>
-    //                         <th>Grados</th>
-    //                         <th>Mensaje</th>
-    //                         <th>Edad</th>
-    //                     </thead>
-    //                     <tbody>
-    //                         <tr>
-    //                             <td>${email}</td>
-    //                             <td>${nombre}</td>
-    //                             <td>${fecha}</td>
-    //                             <td>${rango}</td>
-    //                             <td>${genero}</td>
-    //                             <td>
-    //                                 <ol>
-    //                                     ${gradosStr}
-    //                                 </ol>
-    //                             </td>
-    //                             <td>${mensaje}</td>
-    //                             <td>${edad}</td>
-    //                         </tr>
-    //                     </tbody>
-    //                 </table>`,
-    //     Port: "2525"
-    // }).then(
-    //     message => alert(message)
-    // ).catch(e => {
-    //     console.log(e);
-    // });
+    await Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "eliasgabriel6100@gmail.com",
+        Password: "C94FBD417510DC4B19F0074F32A2E4DF6336",
+        To: 'eliasgabriel6100@gmail.com',
+        From: "nodemailerproyecto@gmail.com",
+        Subject: "This is the subject",
+        Body: `<table border="1">
+                        <thead>
+                            <th>Email</th>
+                            <th>Nombre</th>
+                            <th>Fecha</th>
+                            <th>Rango</th>
+                            <th>Genero</th>
+                            <th>Grados</th>
+                            <th>Mensaje</th>
+                            <th>Edad</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>${email}</td>
+                                <td>${nombre}</td>
+                                <td>${fecha}</td>
+                                <td>${rango}</td>
+                                <td>${genero}</td>
+                                <td>
+                                    <ol>
+                                        ${gradosStr}
+                                    </ol>
+                                </td>
+                                <td>${mensaje}</td>
+                                <td>${edad}</td>
+                            </tr>
+                        </tbody>
+                    </table>`,
+        Port: "2525"
+    }).then(
+        message => {
+            if(message == 'OK') {
+                alert("Mensaje Enviado");
+            }
+            else{
+                alert("Error al Enviar Mensaje");
+            }
+        }
+    ).catch(e => {
+        console.log(e);
+    });
+
 }
